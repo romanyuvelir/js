2953a237f7772c2844c971ebcaaee2981c5bafdf
romanyuvelir <romanshelukhov@gmail.com>
2025-05-04 00:38:14 +0300

    lab3

--- lab3.js
+++ lab3.js
@@ -10,6 +10,21 @@ function random(min, max) {
     return true;
   }
   
+  function sumTo(n) {
+    let sum = 0;
+    if (n > 0) {
+      for (let i = 1; i <= n; i++) sum += i;
+    } else {
+      for (let i = 1; i >= n; i--) sum += i;
+    }
+    return sum;
+  }
+  
+  // Приклад використання:
+  console.log(sumTo(5));   // 15 (1+2+3+4+5)
+  console.log(sumTo(-3));  // -5 (1 + 0 + (-1) + (-2) + (-3))
+  
+
   // Приклад використання:
   console.log(isPrime(7));  // true
   console.log(isPrime(10)); // false
