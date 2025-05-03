f9b74a65a7fe20211adcc0fc621540b1fa5560b1
romanyuvelir <romanshelukhov@gmail.com>
2025-05-04 00:21:34 +0300

    lab1

---
+++ mai.js
@@ -0,0 +1,18 @@
+let login = prompt("Введіть логін:");
+
+if (login === "Admin") {
+    let password = prompt("Введіть пароль:");
+
+    if (password === "Господар") {
+        alert("Ласкаво просимо!");
+    } else if (password === "" || password === null) {
+        alert("Скасовано");
+    } else {
+        alert("Неправильний пароль");
+    }
+
+} else if (login === "" || login === null) {
+    alert("Скасовано");
+} else {
+    alert("Я вас не знаю");
+}
